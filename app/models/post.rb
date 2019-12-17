6522016651e99fe98969d2b454a39a5cf2e45dba
github username <github email address>
2019-12-17 17:41:24 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction fiction Non-Fiction nonfiction non-fiction) }
+    validate :clickbaity
+
+    def clickbaity
+        if self.title && !self.title.downcase.include?("won't believe" || "secret" || "guess")
+            self.errors.add(:title, "has to include 'won't believe' 'secret' 'top [number]' 'guess'")
+
+
+    end
+
+end
 end
